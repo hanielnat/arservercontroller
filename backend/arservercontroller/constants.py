920862311860abc81110c92142ba426253e90504
hnl <haniel.nat7220@gmail.com>
2025-08-02 22:47:18 +0000

    add: default paths for directories

--- backend/arservercontroller/constants.py
+++ backend/arservercontroller/constants.py
@@ -1,4 +1,24 @@
 import enum
+import pathlib
+from pathlib import Path
+
+from pydantic import BaseModel
+
+import arservercontroller
+
+
+class BaseDirectories(BaseModel):
+    MODULE_DIR: Path = pathlib.Path(arservercontroller.__file__)
+    ROOT_DIR: Path = MODULE_DIR.parent
+    DATA_DIR: Path = ROOT_DIR / "data"
+    LOGS_DIR: Path = ROOT_DIR / "logs"
+
+
+class ControllerDirectories(BaseDirectories):
+    CONTROLLER_DIR: Path = BaseDirectories.DATA_DIR / "controller"
+    DS_CONFIGS_DIR: Path = CONTROLLER_DIR / "ds_configs"
+    DS_PROFILES_DIR: Path = CONTROLLER_DIR / "profiles"
+    CONTAINER_VOLUMES_DIR: Path = CONTROLLER_DIR / "volumes"
 
 
 class EnumARServerStatus(enum.Enum):
