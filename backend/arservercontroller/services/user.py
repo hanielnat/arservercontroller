62ebc1c359b83037043afd751881e549fd877961
hnl <haniel.nat7220@gmail.com>
2025-10-10 05:29:00 +0000

    backend: add `UserService`

--- backend/arservercontroller/services/user.py
+++ backend/arservercontroller/services/user.py
@@ -1,14 +1,105 @@
+from datetime import timedelta
+from typing import Annotated, Optional
+
+import jwt
+from fastapi import Depends, HTTPException, status
+from jwt.exceptions import InvalidTokenError
+from pydantic import EmailStr, ValidationError
+
+from arservercontroller.api.dependencies import (
+    DbSessionDep,
+    OAuth2FormDep,
+    OAuth2TokenDep,
+)
+from arservercontroller.core.config import get_config
+from arservercontroller.core.security import (
+    ALGORITHM,
+    create_access_token,
+    verify_password,
+)
 from arservercontroller.db.models.user import User
-from arservercontroller.db.repositories.user_repo import UserRepository
-from arservercontroller.schemas.user import UserOut, UserRegister
+from arservercontroller.schemas.user import (
+    Token,
+    TokenData,
+    UserLogin,
+    UserOut,
+    UserRegister,
+)
+
+_settings = get_config()
 
 
 class UserService:
-    def __init__(self, user_repo: UserRepository):
-        self.user_repo = user_repo
+    def __init__(self, db: DbSessionDep):
+        self._db = db
+        self._unauthorized_exception = HTTPException(
+            status.HTTP_401_UNAUTHORIZED,
+            "Invalid user credentials",
+            {"WWW-Authenticate": "Bearer"},
+        )
 
-    def register_user(self, user_model: UserRegister) -> UserOut:
-        if self.user_repo.find_by_email(user_model.email):
-            raise ValueError("Email already exists.")
-        new_user = User(email=user_model.email)
-        return self.user_repo.add(new_user)
+    async def find_by_id(self, id: int) -> Optional[User]:
+        return self._db.get(User, id)
+
+    async def find_by_name(self, name: str) -> Optional[User]:
+        return self._db.query(User).filter(User.name == name).first()
+
+    async def find_by_email(self, email: EmailStr) -> Optional[User]:
+        return self._db.query(User).filter(User.email == email).first()
+
+    async def auth_user(self, user: UserLogin) -> User:
+        model = await self.find_by_name(user.name)
+        if not model:
+            raise self._unauthorized_exception
+
+        if not verify_password(user.password, model.hashed_password):
+            raise self._unauthorized_exception
+
+        return model
+
+    async def get_current_user(self, token: OAuth2TokenDep) -> UserOut:
+        try:
+            payload = jwt.decode(token, _settings.SECRET_KEY, algorithms=[ALGORITHM])
+            token_data = TokenData(**payload)
+        except (InvalidTokenError, ValidationError):
+            raise self._unauthorized_exception
+
+        user = await self.find_by_name(str(token_data.username))
+        if user is None:
+            raise self._unauthorized_exception
+
+        return UserOut.model_validate(user)
+
+    async def get_current_admin_user(self, token: OAuth2TokenDep) -> Optional[UserOut]:
+        user = await self.get_current_user(token)
+        if user.role != "admin":
+            return None
+        return user
+
+    async def register_user(self, new_user: UserRegister) -> UserOut:
+        if await self.find_by_name(new_user.name):
+            raise HTTPException(status.HTTP_403_FORBIDDEN, "User name already exists")
+
+        user_model = User(
+            name=new_user.name, email=new_user.email, hashed_password=new_user.password
+        )
+        self._db.add(user_model)
+        self._db.commit()
+        self._db.refresh(user_model)
+
+        return UserOut.model_validate(user_model)
+
+    async def login_user(self, form_data: OAuth2FormDep) -> Token:
+        user_form = UserLogin(name=form_data.username, password=form_data.password)
+        model = await self.auth_user(user_form)
+        return await self.create_access_token({"username": model.name})
+
+    async def create_access_token(self, data: dict) -> Token:
+        token_expires_delta = timedelta(minutes=_settings.JWT_EXPIRE_MINUTES)
+        token = create_access_token(data, token_expires_delta)
+        return Token(access_token=token, token_type="bearer")
+
+
+UserServiceDep = Annotated[UserService, Depends(UserService.__init__)]
+CurrentUserDepV2 = Annotated[UserOut, Depends(UserService.get_current_user)]
+CurrentAdminUserDep = Annotated[UserOut, Depends(UserService.get_current_admin_user)]
