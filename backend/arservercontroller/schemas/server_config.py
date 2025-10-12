e6598ba25b6b145929c9c20b2a529a870c483d82
hnl <haniel.nat7220@gmail.com>
2025-10-12 06:52:45 +0000

    backend: improve `ServerConfig` model serialization and `Field` s

--- backend/arservercontroller/schemas/server_config.py
+++ backend/arservercontroller/schemas/server_config.py
@@ -2,12 +2,13 @@ import uuid
 from ipaddress import IPv4Address
 from typing import Annotated, Optional
 
-from docker.models.containers import Container
 from pydantic import (
     UUID4,
     BaseModel,
+    ConfigDict,
     Field,
     IPvAnyAddress,
+    field_serializer,
     field_validator,
 )
 
@@ -18,105 +19,104 @@ _PORT_MIN: int = 0
 
 
 class ServerConfigBase(BaseModel):
+    model_config = ConfigDict(use_enum_values=True, str_max_length=255)
+
     # fmt: off
     version: Annotated[
-        str, Field(default=SERVER_SCHEMA_VERSION)
+        str,
+        Field(SERVER_SCHEMA_VERSION, frozen=True)
     ]
 
     name: Annotated[
-        str, Field(min_length=4, max_length=20)
+        str,
+        Field(min_length=4, max_length=20)
     ]
 
     bind_port: Annotated[
-        int, Field(default=2001, gt=_PORT_MIN, lt=_PORT_MAX)
+        int,
+        Field(2001, gt=_PORT_MIN, lt=_PORT_MAX)
     ]
 
     bind_address: Annotated[
-        Optional[IPvAnyAddress], Field(default=IPv4Address("0.0.0.0"))
+        Optional[IPvAnyAddress],
+        Field(IPv4Address("0.0.0.0"))
     ]
 
     a2s_port: Annotated[
-        Optional[int], Field(default=17777, gt=_PORT_MIN, lt=_PORT_MAX)
+        Optional[int],
+        Field(17777, gt=_PORT_MIN, lt=_PORT_MAX)
     ]
 
     rcon_port: Annotated[
-        Optional[int], Field(default=19999, gt=_PORT_MIN, lt=_PORT_MAX)
+        Optional[int],
+        Field(19999, gt=_PORT_MIN, lt=_PORT_MAX)
     ]
 
     status: Annotated[
-        ServerStatusEnum, Field(default=ServerStatusEnum.DEAD, exclude=True)
+        ServerStatusEnum,
+        Field(ServerStatusEnum.DEAD, validate_default=True)
     ]
 
     command_line: Annotated[
-        Optional[list[str] | str], Field(default=None)
+        Optional[list[str] | str],
+        Field(None)
     ]
 
     arserver_profile_path: Annotated[
-        Optional[str], Field(default=None)
+        Optional[str],
+        Field(None)
     ]
 
     arserver_config_path: Annotated[
-        Optional[str], Field(default=None)
+        Optional[str],
+        Field(None)
     ]
     # fmt: on
-
-    @classmethod
-    @field_validator("version")
-    def schema_version(cls, v: str) -> str:
-        if v != SERVER_SCHEMA_VERSION:
-            raise ValueError(
-                "JSON schema version entered does not match!got: '{}', must be: '{}'".format(
-                    v, SERVER_SCHEMA_VERSION
-                )
-            )
-        return v
-
-    # @field_serializer("bind_address", when_used="json")
-    # def serialize_bind_address(self, bind_address: IPv4Address) -> Optional[str]:
-    #     return str(bind_address) if bind_address else None
-
-    # @field_serializer("status", when_used="json")
-    # def serialize_status(self, status: ServerStatusEnum) -> str:
-    #     return status.value
 
 
 class ServerConfigCreate(ServerConfigBase):
     status: Annotated[
-        ServerStatusEnum, Field(default=ServerStatusEnum.CREATED, exclude=True)
+        ServerStatusEnum,
+        Field(ServerStatusEnum.CREATED, exclude=True, validate_default=True),
     ]
 
 
 class ServerConfigUpdate(ServerConfigBase):
     # fmt: off
     container_id: Annotated[
-        Optional[str], Field(default=None)
-    ]
+        Optional[str],
+        Field(None)
+    ] = None
 
     name: Annotated[  # pyright: ignore[reportIncompatibleVariableOverride]
         Optional[str],
-        Field(default=None, min_length=4, max_length=20)
-    ]
+        Field(None, min_length=4, max_length=20)
+    ] = None
 
     bind_port: Annotated[  # pyright: ignore[reportIncompatibleVariableOverride]
-        Optional[int], Field(default=None, gt=_PORT_MIN, lt=_PORT_MAX)
-    ]
+        Optional[int],
+        Field(None, gt=_PORT_MIN, lt=_PORT_MAX)
+    ] = None
 
     bind_address: Annotated[
-        Optional[IPvAnyAddress], Field(default=None)
-    ]
+        Optional[IPvAnyAddress],
+        Field(None)
+    ] = None
 
     a2s_port: Annotated[
-        Optional[int], Field(default=None, gt=_PORT_MIN, lt=_PORT_MAX)
-    ]
+        Optional[int],
+        Field(None, gt=_PORT_MIN, lt=_PORT_MAX)
+    ] = None
 
     rcon_port: Annotated[
-        Optional[int], Field(default=None, gt=_PORT_MIN, lt=_PORT_MAX)
-    ]
+        Optional[int],
+        Field(None, gt=_PORT_MIN, lt=_PORT_MAX)
+    ] = None
 
     status: Annotated[  # pyright: ignore[reportIncompatibleVariableOverride]
         Optional[ServerStatusEnum],
-        Field(default=None, exclude=True)
-    ]
+        Field(None, exclude=True, validate_default=True)
+    ] = None
     # fmt: on
 
 
@@ -124,13 +124,21 @@ class ServerConfig(ServerConfigBase):
     id: Annotated[UUID4, Field(default_factory=uuid.uuid4)]
     container_id: Annotated[str, Field()]
 
-    def update_status(self, container: Container):
-        if container.id != self.container_id:
-            return
+    @classmethod
+    @field_validator("version")
+    def schema_version(cls, v: str) -> str:
+        if v != SERVER_SCHEMA_VERSION:
+            raise ValueError(
+                "JSON schema version entered does not match! got: '{}', must be: '{}'".format(
+                    v, SERVER_SCHEMA_VERSION
+                )
+            )
+        return v
 
-        container.reload()
-        self.status = ServerStatusEnum[container.status.upper()]
+    @field_serializer("bind_address")
+    def serialize_bind_address_maybe(self, v: IPvAnyAddress) -> Optional[str]:
+        return str(v) if v else None
 
-    # @field_serializer("id", when_used="json")
-    # def serialize_id(self, id: UUID4) -> str:
-    #     return str(id)
+    @field_serializer("id")
+    def id_to_string(self, v: UUID4) -> str:
+        return str(v)
