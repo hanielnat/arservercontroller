de3883eb2123f1cb73f738d1916985276e25243d
hnl <haniel.nat7220@gmail.com>
2025-10-14 03:14:19 +0000

    frontend: add login/register base pages

---
+++ frontend/app/pages/register.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const form = reactive({
+    username: "",
+    password: ""
+})
+</script>
+
+<template>
+    <UPageBody class="flex place-items-center">
+        <UForm
+            schema="form"
+            class="p4"
+        >
+            <UInput>
+                <UFormField label="User name"></UFormField>
+            </UInput>
+            <UFormField label="Password"></UFormField>
+        </UForm>
+    </UPageBody>
+</template>
