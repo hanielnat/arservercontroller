a243cd4fbeb19be930858ed804b7ca2f2fed0c9e
hnl <haniel.nat7220@gmail.com>
2026-04-02 21:01:31 -0300

    frontend: add new `useApi` composable

---
+++ frontend/src/composables/useApi.ts
@@ -0,0 +1,14 @@
+import config from "../config"
+
+export const useApi = () =>
+{
+    const apiUrl = (path: string) =>
+        `${config.API_BASE_URL}${config.API_V1}${path}`
+    const wsUrl = (path: string) =>
+        `${config.WS_BASE_URL}${config.API_V1}${path}`
+
+    return {
+        apiUrl,
+        wsUrl,
+    }
+}
